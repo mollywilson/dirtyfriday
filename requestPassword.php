270e274dc4cac4f23c456c95659f3ee6a196d7de
Unknown <mollywilson_@outlook.com>
2018-08-07 16:26:08 +0100

    clean up requestPassword.php

--- requestPassword.php
+++ requestPassword.php
@@ -7,7 +7,6 @@
     $url = "http://molly.localhost/dirtyFriday/resetPassword.php";
     $admin_name = "Dirty Fridays";
     $admin_email = "molly@Mollys-MBP.magmadigital.co.uk";
-    $message = "Please click the link to change your password!" . "\n";
 
 ?>
 
@@ -20,39 +19,35 @@
     </form>
 </html>
 
-
 <?php
 
-    if (isset($_POST['submitted'])) { //if form submitted
+function requestPassword() {
 
-        include 'inc/filter.php';
+    global $conn;
+    include 'inc/filter.php';
+    $errors = [];
+    $email = filter($_POST['email']);
+    $result = mysqli_query($conn, "SELECT * FROM logIn WHERE name='".filter($_POST['username'])."' AND email='".filter($_POST['email'])."'");
+    $email_hash = password_hash(filter($_POST['email']), PASSWORD_BCRYPT);
+    $link = "http://molly.localhost/dirtyFriday/resetPassword.php?key=".$email_hash."";
+    $_SESSION["email"] = filter($_POST['email']);
 
-        $name = filter($_POST['username']);
-        $email = filter($_POST['email']);
+    if ((empty(filter($_POST['username']))) || (empty(filter($_POST['email'])))) {
+        $errors[] = "Please enter your username and email address";
+    }
 
-        $sql = "SELECT * FROM logIn WHERE name='".$name."' AND email='".$email."'";
-        $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) == 0) {
+        $errors[] = "Your username or password is incorrect";
+    }
 
-        if ((!empty($name)) && (!empty($email))) { //if not empty
+    if (!empty($errors)) {
+        echo $errors[0];
+    } else {
+        mail("$email", 'Dirty Fridays: Forgot my Password', "Please click the link to change your password!" . "$link");
+        echo "Your email has been sent!";
+    }
+}
 
-            if (mysqli_num_rows($result) == 1) { //if name and email match
-
-                while ($row = mysqli_fetch_array($result)) {
-                    $email_hash = password_hash($email, PASSWORD_BCRYPT);
-                }
-
-                $link = "http://molly.localhost/dirtyFriday/resetPassword.php?key=".$email_hash."";
-
-                $_SESSION["email"] = $email;
-
-                mail("$email", "Dirty Fridays: Forgot my Password", "$message" . "$link");
-                echo "Your email has been sent!";
-            }//end of if name and email match
-            else {
-                echo "Your name or email address is incorrect!";
-            }//end of name and email address else
-        } //end of if not empty
-        else {
-            echo "Please fill in your details";
-        }//end of if not empty else
-    }//end of form submitted
+if (isset($_POST['submitted'])) {
+    requestPassword();
+}
--- test.php
+++ test.php
@@ -1,54 +1,70 @@
 <?php
-include 'inc/connect.php';
-$greeting = "Place your order " . $_SESSION["username"] . "!";
-include 'inc/header.php';
 
-function order() {
-
-    global $conn;
-    $errors = [];
-    include 'inc/filter.php';
-
-    if (empty(filter($_POST['order']))) {
-        $errors[] = "Please enter an order!";
-    }
-
-    if (!empty($errors)) {
-        echo $errors[0];
-    } else {
-        mysqli_query($conn, "INSERT INTO foodOrders (name, food, date) VALUES ('".$_SESSION['username']."', '".filter($_POST['order'])."', NOW())");
-        header("location: orders.php");
-    }
-}
-
-if (isset($_POST['submitted'])) {
-    order();
-}
-?>
-<?php
-include 'inc/connect.php';
-$greeting = "Place your order " . $_SESSION["username"] . "!";
-include 'inc/header.php';
-
-if (isset($_POST['submitted'])) {
+if (isset($_POST['submitted'])) { //if form submitted
 
     include 'inc/filter.php';
 
-    $order = filter($_POST['order']);
+    $name = filter($_POST['username']);
+    $email = filter($_POST['email']);
 
-    if (!empty($order)) {
+    $sql = "SELECT * FROM logIn WHERE name='".$name."' AND email='".$email."'";
+    $result = mysqli_query($conn, $sql);
 
-        $sql2 = "INSERT INTO foodOrders (name, food, date) VALUES 
-                    ('".$_SESSION['username']."', '$order', NOW())";
+    if ((!empty($name)) && (!empty($email))) { //if not empty
 
-        if (!mysqli_query($conn, $sql2)) {
-            die('Your order has NOT been placed.');
-        } else {
-            header("location: orders.php");
-        } //end of if order placed else
-    } //end of if empty
+        if (mysqli_num_rows($result) == 1) { //if name and email match
+
+            while ($row = mysqli_fetch_array($result)) {
+                $email_hash = password_hash($email, PASSWORD_BCRYPT);
+            }
+
+            $link = "http://molly.localhost/dirtyFriday/resetPassword.php?key=".$email_hash."";
+
+            $_SESSION["email"] = $email;
+
+            mail("$email", "Dirty Fridays: Forgot my Password", "$message" . "$link");
+            echo "Your email has been sent!";
+        }//end of if name and email match
+        else {
+            echo "Your name or email address is incorrect!";
+        }//end of name and email address else
+    } //end of if not empty
     else {
-        echo "Please fill in your name and order";
-    } //end of if empty else
-} //end of isset
+        echo "Please fill in your details";
+    }//end of if not empty else
+}//end of form submitted
+
 ?>
+
+<?php
+
+    function requestPassword() {
+
+        global $conn;
+        include 'inc/filter.php';
+        $errors = [];
+        $email = filter($_POST['email']);
+        $result = mysqli_query($conn, "SELECT * FROM logIn WHERE name='".filter($_POST['username'])."' AND email='".filter($_POST['email'])."'");
+        $email_hash = password_hash(filter($_POST['email']), PASSWORD_BCRYPT);
+        $link = "http://molly.localhost/dirtyFriday/resetPassword.php?key=".$email_hash."";
+        $_SESSION["email"] = filter($_POST['email']);
+
+        if ((empty(filter($_POST['username']))) || (empty(filter($_POST['email'])))) {
+            $errors[] = "Please enter your username and email address";
+        }
+
+        if (mysqli_num_rows($result) == 0) {
+            $errors[] = "Your username or password is incorrect";
+        }
+
+        if (!empty($errors)) {
+            echo $errors[0];
+        } else {
+            mail("$email", 'Dirty Fridays: Forgot my Password', "$message" . "$link");
+            echo "Your email has been sent!";
+        }
+    }
+
+    if (isset($_POST['submitted'])) {
+        requestPassword();
+    }
